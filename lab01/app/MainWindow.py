66561931c61414514d4cbb0457f871e08173be8e
Mike <darkcore27@gmail.com>
2016-01-25 16:25:39 +0200

    lab1 done

--- lab01/app/MainWindow.py
+++ lab01/app/MainWindow.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import math
+
+import numpy as np
 
 sys.path.append(os.path.abspath(os.path.dirname(__name__))+'/app')
 
@@ -20,28 +23,120 @@ class MainWindow(QtWidgets.QWidget, Ui_Form):
 
     def connect_buttons(self):
         self.draw_lab0_btn.clicked.connect(self._draw_frame)
+        self.transform_lab1_afin_btn.clicked.connect(self.affine_morf_action)
+        self.move_evk_btn.clicked.connect(self.move_action)
+        self.rotate_evk_btn.clicked.connect(self.rotate_action)
+        self.draw_lab1_afin_btn.clicked.connect(self._draw_frame)
+        self.draw_lab1_transform_btn.clicked.connect(self._draw_frame)
+        self.transform_lab1_transform_btn.clicked.connect(self.transform_action)
 
     def set_pixmap(self):
         self.label.setPixmap(QtGui.QPixmap("figure.jpg"))
 
     def _draw_frame(self):
+        self.graphicsView.clear_scene()
         self._draw_grid()
         self._draw_figure()
 
-    def _draw_figure(self):
+    def _draw_figure(self, modifier=lambda x, y: (x, y)):
         try:
             R = int(self.big_rad_ledit.text() or '20')
             r = int(self.lit_rad_ledit.text() or '10')
             r1 = int(self.ver_rad_ledit.text() or '20')
             r2 = int(self.hor_rad_ledit.text() or '20')
             D = int(self.diag_size_ledit.text() or '100')
-            self.graphicsView.draw_figure(D, r1, r2, r, R)
+            self.graphicsView.draw_figure(D, r1, r2, r, R, modifier=modifier)
         except Exception as e:
-            self.label_7.setText(self._translate("Form", str(e)))
+            pass
 
-    def _draw_grid(self):
+    def _draw_grid(self, modifier=lambda x, y: (x, y)):
         try:
             H = int(self.diag_size_ledit.text() or '100') * 1.2
-            self.graphicsView.draw_grid(int(H))
+            self.graphicsView.draw_grid(int(H), modifier=modifier)
         except Exception as e:
-            self.label_7.setText(self._translate("Form", str(e)))
+            pass
+
+    def affine_morf_action(self):
+        print('Hao')
+        try:
+            affine_matrix = np.matrix([
+                [float(self.rx_0_afin_ledit.text() or '1'),
+                 float(self.rx_1_afin_ledit.text() or '0'),
+                 0],
+                [float(self.ry_0_afin_ledit.text() or '0'),
+                 float(self.ry_1_afin_ledit.text() or '1'),
+                 0],
+                [float(self.r0_0_afin_ledit.text() or '0'),
+                float(self.r0_1_afin_ledit.text() or '0'),
+                1]
+            ])
+            self.graphicsView.clear_scene()
+            self._draw_grid(modifier=lambda x, y: ((np.matrix([x, y, 1]) * affine_matrix).tolist())[0][:2])
+            self._draw_figure(modifier=lambda x, y: ((np.matrix([x, y, 1]) * affine_matrix).tolist())[0][:2])
+        except Exception:
+            raise
+
+
+    def move_action(self):
+        try:
+            move_matrix = np.matrix([
+                [1, 0, 0],
+                [0, 1, 0],
+                [float(self.mv_axis_x_evk_ledit.text() or '0'),
+                 float(self.mv_axis_y_evk_ledit.text() or '0'),
+                 0]
+            ])
+            self.graphicsView.clear_scene()
+            self._draw_grid()
+            self._draw_figure(modifier=lambda x, y: (np.matrix([x, y, 1]) * move_matrix).tolist()[0][:2])
+        except Exception:
+            pass
+
+    def rotate_action(self):
+        try:
+            x = float(self.rt_x_center_evk_ledit.text() or '0')
+            y = float(self.rt_y_center_evk_ledit.text() or '0')
+            angle = math.radians(float(self.rt_angle_evk_ledit.text() or '0'))
+
+            reverse_m = np.matrix([
+                [1, 0, 0],
+                [0, 1, 0],
+                [-x, -y, 1]
+            ])
+            move_m = np.matrix([
+                [1, 0, 0],
+                [0, 1, 0],
+                [x, y, 1]
+            ])
+            rotate_m = np.matrix([
+                [math.cos(angle), math.sin(angle), 0],
+                [-math.sin(angle), math.cos(angle), 0],
+                [0, 0, 1]
+            ])
+
+            self.graphicsView.clear_scene()
+            self._draw_grid()
+            self._draw_figure(modifier=lambda x, y: (np.matrix([x, y, 1]) * reverse_m * rotate_m * move_m).tolist()[0][:2])
+        except Exception:
+            pass
+
+    def transform_action(self):
+        try:
+            transform_m = np.matrix([
+                [float(self.vec_x_0_ledit.text() or '1'),
+                 float(self.vec_x_1_ledit.text() or '0'),
+                 float(self.vec_x_2_ledit.text() or '0')],
+                [float(self.vec_y_0_ledit.text() or '0'),
+                 float(self.vec_y_1_ledit.text() or '1'),
+                 float(self.vec_y_2_ledit.text() or '0')],
+                [float(self.vec_r0_0_ledit.text() or '0'),
+                 float(self.vec_r0_1_ledit.text() or '0'),
+                 float(self.vec_r0_2_ledit.text() or '1')]
+            ])
+
+            self.graphicsView.clear_scene()
+            modf =modifier=lambda x, y: (np.matrix([x, y, 1]) * transform_m).tolist()[0][:2]
+            self._draw_grid(modifier=modf)
+            self._draw_figure(modifier=modf)
+        except Exception as e:
+            pass
--- lab01/app/mygraphicsview.py
+++ lab01/app/mygraphicsview.py
@@ -9,38 +9,48 @@ class MyGraphicsView(QtWidgets.QGraphicsView):
 
         self.scene = QtWidgets.QGraphicsScene(self)
 
+    def clear_scene(self):
+        self.scene.clear()
+
     def draw_frame(self, h, r_vert, r_hor, r_in, r_out, modifier=lambda x: x):
         self.h2 = h / 2
         if h > 0 and r_vert > 0 and r_hor > 0 and r_in > 0 and r_out > 0 and \
                 math.sqrt((self.h2-r_vert) ** 2 / (self.h2 ** 2 + (self.h2-r_vert) ** 2)) * self.h2 >= r_out and \
                 self.h2-r_vert > 0 and self.h2 - r_hor > 0 and r_in < r_out:
             self.scene.clear()
-            self.draw_grid(grid_size=int(1.2*h))
-            self.draw_figure(h, r_vert, r_hor, r_in, r_out)
+            self.draw_grid(grid_size=int(1.2*h), modifier=modifier)
+            self.draw_figure(h, r_vert, r_hor, r_in, r_out, modifier=modifier)
 
-    def draw_grid(self, grid_size=100):
+    def draw_grid(self, grid_size=100, modifier=lambda x, y: (x, y)):
         X = self._convert_x(grid_size)
         _X = self._convert_x(-grid_size)
         Y = self._convert_y(grid_size)
         _Y = self._convert_y(-grid_size)
         for x in range(-grid_size, grid_size + 1, 30):
             _x = self._convert_x(x)
+            _xt1, _Yt = modifier(_x, _Y)
+            _xt2, Yt = modifier(_x, Y)
             text = QtWidgets.QGraphicsTextItem()
-            text.setPos(_x - 25, Y - 30)
+            text.setPos(*(modifier(_x - 25, Y - 30)))
             text.setPlainText('{:>5}'.format(x))
-            self.scene.addLine(_x, _Y, _x, Y)
+            self.scene.addLine(_xt1, _Yt, _xt2 , Yt)
             self.scene.addItem(text)
         for y in range(-grid_size, grid_size + 1, 30):
             _y = self._convert_y(y)
+            _Xt, _yt1 = modifier(_X, _y)
+            Xt, _yt2 = modifier(X, _y)
             text = QtWidgets.QGraphicsTextItem()
-            text.setPos(_X - 50, _y - 10)
-            text.setPlainText('{:>5}'.format(y))
-            self.scene.addLine(_X, _y, X, _y)
+            text.setPos(*(modifier(_X - 50, _y - 10)))
+            text.setPlainText('{:>5}'.format(-y))
+            self.scene.addLine(_Xt, _yt1, Xt, _yt2)
             self.scene.addItem(text)
 
-    def draw_figure(self, h, r_vert, r_hor, r_in, r_out, modifier=lambda x: x):
+    def draw_figure(self, h, r_vert, r_hor, r_in, r_out,
+            modifier=lambda x, y: (x, y)):
         pen = QtGui.QPen(QtCore.Qt.red)
         for x1, y1, x2, y2 in self._create_item(h, r_vert, r_hor, r_in, r_out):
+            x1, y1 = modifier(x1, y1)
+            x2, y2 = modifier(x2, y2)
             self.scene.addLine(x1, y1, x2, y2, pen)
         self.setScene(self.scene)
 
@@ -56,7 +66,7 @@ class MyGraphicsView(QtWidgets.QGraphicsView):
                 x, y = xt, yt
 
     def _convert_x(self, x):
-        return x + self.width()/2-20
+        return x
 
     def _convert_y(self, y):
-        return -y + self.height()/2 - 120
+        return -y
